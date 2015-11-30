659d1979e26ee16c8867a25b9a55f10ce98721ee
Ryan Parsley <ryan.parsley@rockfishinteractive.com>
2015-11-30 15:51:29 -0500

    set up copy of assets from src to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,15 +5,41 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    src: {
+      root: 'src',
+      jsDir: 'src/scripts/',
+      imgDir: 'src/images/',
+      sassDir: 'src/styles/',
+      sass: ['<%= src.sassDir %>main.scss']
+    },
+    dist: {
+      root: 'dist',
+      jsDir: 'dist/assets/scripts/',
+      imgDir: 'dist/assets/images/',
+      cssDir: 'dist/assets/styles/',
+      css: ['<%= dist.cssDir %>main.css']
+    },
     jshint: {
-      all: ['Gruntfile.js', 'templates/helpers/*.js'],
+      all: ['Gruntfile.js', '<%= src.jsDir %>**/*.js'],
       options: {
         jshintrc: '.jshintrc'
       }
     },
+    copy: {
+      js: {
+        files: [
+          {expand: true, cwd: '<%= src.jsDir %>', src: ['**/*.js'], dest: '<%= dist.jsDir %>app.js'}
+        ],
+      },
+      html: {
+        files: [
+          {expand: true, cwd: '<%= src.root %>', src: ['**/*.html'], dest: '<%= dist.root %>'}
+        ],
+      },
+    },
     watch: {
       all: {
-        files: ['Gruntfile.js', 'src/scripts/**/*.js', 'src/styles/**/*.scss', 'src/images/**/*.*'],
+        files: ['Gruntfile.js', '<%= src.jsDir %>**/*.js', '<%= src.sassDir %>**/*.scss', '<%= src.imgDir %>**/*.*'],
         tasks: ['default'],
         options: {
           atBegin: true,
@@ -22,17 +48,17 @@ module.exports = function(grunt) {
       }
     },
     sass: {
-        options: {
-            sourceMap: true
-        },
-        dist: {
-            files: {
-                'dist/assets/styles/main.css': 'src/styles/main.scss'
-            }
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          '<%= dist.cssDir %>main.css': '<%= src.sass %>'
         }
+      }
     }
   });
 
-  grunt.registerTask('default', ['jshint', 'sass']);
+  grunt.registerTask('default', ['jshint', 'sass', 'copy']);
 
 };
